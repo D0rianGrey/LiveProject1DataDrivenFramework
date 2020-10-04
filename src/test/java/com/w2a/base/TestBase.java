0849c531241a6874f59c1977e5f7dd6529c29335
Vakerin Eugene <utyfdfrthby08@gmail.com>
2020-10-04 16:13:46 +0300

    adding properties and first test ng test

--- src/test/java/com/w2a/base/TestBase.java
+++ src/test/java/com/w2a/base/TestBase.java
@@ -1,25 +1,30 @@
 package com.w2a.base;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
-    private static WebDriver driver;
-    private static Properties config = new Properties();
-    private static Properties OR = new Properties();
-    private static FileInputStream fis;
+    protected static WebDriver driver;
+    protected static Properties config = new Properties();
+    protected static Properties OR = new Properties();
+    protected static FileInputStream fis;
 
     @BeforeSuite
-    public void setUp(){
-        if (driver==null){
+    public void setUp() {
+        if (driver == null) {
             FileInputStream fis = null;
             try {
-                fis = new FileInputStream(System.getProperty("user.dir")+"/src/test/java/resources/properties/Config.properties");
+                fis = new FileInputStream(System.getProperty("user.dir") + "/src/test/java/resources/properties/Config.properties");
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
@@ -30,7 +35,7 @@ public class TestBase {
             }
 
             try {
-                fis = new FileInputStream(System.getProperty("user.dir")+"/src/test/java/resources/properties/OR.properties");
+                fis = new FileInputStream(System.getProperty("user.dir") + "/src/test/java/resources/properties/OR.properties");
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
@@ -39,14 +44,31 @@ public class TestBase {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            if (config.getProperty("browser").equals("firefox")) {
+                System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\src\\test\\java\\resources\\executables\\geckodriver.exe");
+                driver = new FirefoxDriver();
+
+            } else if (config.getProperty("browser").equals("chrome")) {
+                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\test\\java\\resources\\executables\\chromedriver.exe");
+                driver = new ChromeDriver();
+            }
+
+            driver.get(config.getProperty("testsiteurl"));
+            driver.manage().window().maximize();
+            driver.manage()
+                    .timeouts()
+                    .implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
         }
 
     }
 
 
     @AfterSuite
-    public void tearDown(){
-
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
 
